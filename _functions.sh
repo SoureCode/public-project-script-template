4f5c396c10a9eb4c6e3c8f75cc14e6f474652d32
chapterjason <jason.schilling@hotmail.de>
2025-03-21 22:23:17 +0100

    Add console and database utility functions

--- _functions.sh
+++ _functions.sh
@@ -25,3 +25,58 @@ else
     echo "Missing public-common script-template."
     exit 1
 fi
+
+#<editor-fold desc="binary functions">
+_PUBPST_CONSOLE_BINARY_CACHE=""
+_PUBPST_HAS_CONSOLE=false
+function _pubcst_console() {
+    if [ "$_PUBCST_HAS_SYMFONY" = true ]; then
+        _pubcst_symfony console "$@"
+    else
+        if [ -z "$_PUBPST_CONSOLE_BINARY_CACHE" ]; then
+            _PUBPST_CONSOLE_BINARY_CACHE="${PUBPST_PROJECT_DIRECTORY}/bin/console"
+
+            if [ ! -f "$_PUBPST_CONSOLE_BINARY_CACHE" ]; then
+                echo "missing binary ${_PUBPST_CONSOLE_BINARY_CACHE}"
+                exit 1
+            fi
+
+            _PUBPST_HAS_CONSOLE=true
+        fi
+
+        "$_PUBPST_CONSOLE_BINARY_CACHE" "$@"
+    fi
+}
+#</editor-fold>
+
+#<editor-fold desc="database functions">
+function _pubpst_wait_for_database() {
+    local ATTEMPTS_LEFT_TO_REACH_DATABASE=30
+    local DATABASE_ERROR
+    local EXIT_CODE
+
+    echo "Waiting for database to be ready..."
+
+    while [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -gt 0 ]; do
+        DATABASE_ERROR="$(_console dbal:run-sql -q "SELECT 1" 2>&1)"
+        EXIT_CODE=$?
+
+        if [ "${EXIT_CODE}" -eq 0 ]; then
+            echo "The database is now ready and reachable"
+            return 0
+        elif [ "${EXIT_CODE}" -eq 255 ]; then
+            echo "Unrecoverable error encountered:"
+            echo "$DATABASE_ERROR"
+            exit 1
+        fi
+
+        sleep 1
+        ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE - 1))
+        echo "Still waiting for the database... $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left."
+    done
+
+    echo "The database is not up or not reachable:"
+    echo "$DATABASE_ERROR"
+    exit 1
+}
+#</editor-fold>
